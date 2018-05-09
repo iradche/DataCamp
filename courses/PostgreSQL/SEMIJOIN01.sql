e598bb3be534b7fc35551e1a76aa6e2a437b4809
Irina Radchenko <iradche@gmail.com>
2018-05-09 12:13:07 +0300

    Update SEMIJOIN01.sql

--- courses/PostgreSQL/SEMIJOIN01.sql
+++ courses/PostgreSQL/SEMIJOIN01.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT name   
-FROM languages 
+SELECT DISTINCT name
+FROM languages
 WHERE code IN
-     (SELECT code
-      FROM countries
-      WHERE region = 'Middle East'); 
+  (SELECT code
+   FROM countries
+   WHERE region = 'Middle East')
+ORDER BY name;
