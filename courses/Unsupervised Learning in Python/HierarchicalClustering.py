edd4a9610e1b2993c3ce2e37e797896c996e8bf8
Irina Radchenko <iradche@gmail.com>
2018-05-15 00:12:27 +0300

    Create HierarchicalClustering.py

---
+++ courses/Unsupervised Learning in Python/HierarchicalClustering.py
@@ -0,0 +1,16 @@
+/* Use the linkage() function to obtain a hierarchical clustering of the grain samples, and use dendrogram() to visualize the result. A sample of the grain measurements is provided in the array samples, while the variety of each grain sample is given by the list varieties. */
+
+# Perform the necessary imports
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+# Calculate the linkage: mergings
+mergings = linkage(samples, method='complete')
+
+# Plot the dendrogram, using varieties as labels
+dendrogram(mergings,
+           labels=varieties,
+           leaf_rotation=90,
+           leaf_font_size=6,
+)
+plt.show()
