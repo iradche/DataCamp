c733966cd23be6c59ea75b01a541b83605f7a4bb
Irina Radchenko <iradche@gmail.com>
2018-05-16 23:56:36 +0300

    Create ClusteringWikipediaPart1.py

---
+++ courses/Unsupervised Learning in Python/ClusteringWikipediaPart1.py
@@ -0,0 +1,19 @@
+/* Combine your knowledge of TruncatedSVD and k-means to cluster some popular pages from Wikipedia. In this exercise, build the pipeline. In the next exercise, you'll apply it to the word-frequency array of some Wikipedia articles.
+
+Create a Pipeline object consisting of a TruncatedSVD followed by KMeans. (This time, we've precomputed the word-frequency matrix for you, so there's no need for a TfidfVectorizer).
+
+The Wikipedia dataset you will be working with was obtained from here (https://blog.lateral.io/2015/06/the-unknown-perils-of-mining-wikipedia/). */
+
+# Perform the necessary imports
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cluster import KMeans
+from sklearn.pipeline import make_pipeline
+
+# Create a TruncatedSVD instance: svd
+svd = TruncatedSVD(n_components=50)
+
+# Create a KMeans instance: kmeans
+kmeans = KMeans(n_clusters=6)
+
+# Create a pipeline: pipeline
+pipeline = make_pipeline(svd, kmeans)
