dc1846be6187adc291017b867841852386486a92
Irina Radchenko <iradche@gmail.com>
2018-05-17 22:28:19 +0300

    Create RecommendMusicalArtistsPart1.py

---
+++ courses/Unsupervised Learning in Python/RecommendMusicalArtistsPart1.py
@@ -0,0 +1,24 @@
+/* In this exercise and the next, you'll use what you've learned about NMF to recommend popular music artists! You are given a sparse array artists whose rows correspond to artists and whose column correspond to users. The entries give the number of times each artist was listened to by each user.
+
+In this exercise, build a pipeline and transform the array into normalized NMF features. The first step in the pipeline, MaxAbsScaler, transforms the data so that all users have the same influence on the model, regardless of how many different artists they've listened to. In the next exercise, you'll use the resulting normalized NMF features for recommendation!
+
+This data is part of a larger dataset available here (http://www-etud.iro.umontreal.ca/~bergstrj/audioscrobbler_data.html). */
+# Perform the necessary imports
+from sklearn.decomposition import NMF
+from sklearn.preprocessing import Normalizer, MaxAbsScaler
+from sklearn.pipeline import make_pipeline
+
+# Create a MaxAbsScaler: scaler
+scaler = MaxAbsScaler()
+
+# Create an NMF model: nmf
+nmf = NMF(n_components = 20)
+
+# Create a Normalizer: normalizer
+normalizer = Normalizer()
+
+# Create a pipeline: pipeline
+pipeline = make_pipeline(scaler, nmf, normalizer)
+
+# Apply fit_transform to artists: norm_features
+norm_features = pipeline.fit_transform(artists)
