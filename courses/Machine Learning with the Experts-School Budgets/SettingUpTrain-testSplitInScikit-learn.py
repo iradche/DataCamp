fe2b921fe4169afe53c8d51f37f9beb170f1376d
Irina Radchenko <iradche@gmail.com>
2018-05-20 17:28:27 +0300

    Create SettingUpTrain-testSplitInScikit-learn.py

---
+++ courses/Machine Learning with the Experts-School Budgets/SettingUpTrain-testSplitInScikit-learn.py
@@ -0,0 +1,23 @@
+/* The first step is to split the data into a training set and a test set. Some labels don't occur very often, but we want to make sure that they appear in both the training and the test sets. We provide a function that will make sure at least min_count examples of each label appear in each split: multilabel_train_test_split.
+
+Feel free to check out the full code for multilabel_train_test_split here.
+
+You'll start with a simple model that uses just the numeric columns of your DataFrame when calling multilabel_train_test_split. The data has been read into a DataFrame df and a list consisting of just the numeric columns is available as NUMERIC_COLUMNS. */
+# Create the new DataFrame: numeric_data_only
+numeric_data_only = df[NUMERIC_COLUMNS].fillna(-1000)
+
+# Get labels and convert to dummy variables: label_dummies
+label_dummies = pd.get_dummies(df[LABELS])
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = multilabel_train_test_split(numeric_data_only, label_dummies, size=0.2,seed=123)
+
+# Print the info
+print("X_train info:")
+print(X_train.info())
+print("\nX_test info:")  
+print(X_test.info())
+print("\ny_train info:")  
+print(y_train.info())
+print("\ny_test info:")  
+print(y_test.info()) 
