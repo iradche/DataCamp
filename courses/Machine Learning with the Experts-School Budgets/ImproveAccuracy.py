2e1fe66315976dc4baed5d2cd2c50306b98f201a
Irina Radchenko <iradche@gmail.com>
2018-05-21 22:06:34 +0300

    Create ImproveAccuracy.py

---
+++ courses/Machine Learning with the Experts-School Budgets/ImproveAccuracy.py
@@ -0,0 +1,31 @@
+/* You just saw a substantial improvement in accuracy by swapping out the model. Pipelines are amazing!
+
+Can you make it better? Try changing the parameter n_estimators of RandomForestClassifier(), whose default value is 10, to 15. */
+
+
+# Import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# Add model step to pipeline: pl
+pl = Pipeline([
+        ('union', FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', Imputer())
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+                    ('vectorizer', CountVectorizer())
+                ]))
+             ]
+        )),
+        ('clf', RandomForestClassifier(n_estimators=15))
+    ])
+
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
