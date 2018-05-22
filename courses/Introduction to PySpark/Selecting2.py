7c986dda57ec8db33c60a4ad3a91536fe80b72af
Irina Radchenko <iradche@gmail.com>
2018-05-22 15:53:06 +0300

    Create Selecting2.py

---
+++ courses/Introduction to PySpark/Selecting2.py
@@ -0,0 +1,23 @@
+/*  Similar to SQL, you can also use the .select() method to perform column-wise operations. When you're selecting a column using the df.colName notation, you can perform any column operation and the .select() method will return the transformed column. For example,
+
+flights.select(flights.air_time/60)
+returns a column of flight durations in hours instead of minutes. You can also use the .alias() method to rename a column you're selecting. So if you wanted to .select() the column duration_hrs (which isn't in your DataFrame) you could do
+
+flights.select((flights.air_time/60).alias("duration_hrs"))
+The equivalent Spark DataFrame method .selectExpr() takes SQL expressions as a string:
+
+flights.selectExpr("air_time/60 as duration_hrs")
+with the SQL as keyword being equivalent to the .alias() method. To select multiple columns, you can pass multiple strings.
+
+Remember, a SparkSession called spark is already in your workspace, along with the Spark DataFrame flights. */
+
+
+# Define avg_speed
+avg_speed = (flights.distance/(flights.air_time/60)).alias("avg_speed")
+
+# Select the correct columns
+speed1 = flights.select("origin", "dest", "tailnum", avg_speed)
+
+# Create the same table using a SQL expression
+speed2 = flights.selectExpr("origin", "dest", "tailnum", "distance/(air_time/60) as avg_speed")
+
