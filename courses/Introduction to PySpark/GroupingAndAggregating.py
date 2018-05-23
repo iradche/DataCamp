9eaac26274f870a2061a07984afe85e1079217f8
Irina Radchenko <iradche@gmail.com>
2018-05-23 23:54:01 +0300

    Create GroupingAndAggregating.py

---
+++ courses/Introduction to PySpark/GroupingAndAggregating.py
@@ -0,0 +1,19 @@
+/* Part of what makes aggregating so powerful is the addition of groups. PySpark has a whole class devoted to grouped data frames: pyspark.sql.GroupedData, which you saw in the last two exercises.
+
+You've learned how to create a grouped DataFrame by calling the .groupBy() method on a DataFrame with no arguments.
+
+Now you'll see that when you pass the name of one or more columns in your DataFrame to the .groupBy() method, the aggregation methods behave like when you use a GROUP BY statement in a SQL query!
+
+Remember, a SparkSession called spark is already in your workspace, along with the Spark DataFrame flights. */
+
+# Group by tailnum
+by_plane = flights.groupBy("tailnum")
+
+# Number of flights each plane made
+by_plane.count().show()
+
+# Group by origin
+by_origin = flights.groupBy("origin")
+
+# Average duration of flights from PDX and SEA
+by_origin.avg("air_time").show()
