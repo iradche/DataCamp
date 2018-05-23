85b618ad86139ba26cd3ac6acc93fa567ea80ab0
Irina Radchenko <iradche@gmail.com>
2018-05-23 23:55:47 +0300

    Create Joining2.py

---
+++ courses/Introduction to PySpark/Joining2.py
@@ -0,0 +1,16 @@
+/* In PySpark, joins are performed using the DataFrame method .join(). This method takes three arguments. The first is the second DataFrame that you want to join with the first one. The second argument, on, is the name of the key column(s) as a string. The names of the key column(s) must be the same in each table. The third argument, how, specifies the kind of join to perform. In this course we'll always use the value how="leftouter".
+
+The flights dataset and a new dataset called airports are already in your workspace. */
+
+
+# Examine the data
+print(airports.show())
+
+# Rename the faa column
+airports = airports.withColumnRenamed("faa", "dest")
+
+# Join the DataFrames
+flights_with_airports = flights.join(airports, on="dest", how="leftouter")
+
+# Examine the data again
+print(flights_with_airports.show())
