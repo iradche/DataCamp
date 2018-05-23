42f137180beec362f805791d2ea3cf1d176890b9
Irina Radchenko <iradche@gmail.com>
2018-05-24 00:05:40 +0300

    Create CreateModeler.py

---
+++ courses/Introduction to PySpark/CreateModeler.py
@@ -0,0 +1,7 @@
+/*  The Estimator you'll be using is a LogisticRegression from the pyspark.ml.classification submodule. */
+
+# Import LogisticRegression
+from pyspark.ml.classification import LogisticRegression
+
+# Create a LogisticRegression Estimator
+lr = LogisticRegression()
