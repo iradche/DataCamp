19b71ac0f4080e1c350055ea93375ecaf99d8b2d
Irina Radchenko <iradche@gmail.com>
2018-05-24 00:01:49 +0300

    Create Destination.py

---
+++ courses/Introduction to PySpark/Destination.py
@@ -0,0 +1,7 @@
+/* Now you'll encode the dest column just like you did in the previous exercise. */
+
+# Create a StringIndexer
+dest_indexer = StringIndexer(inputCol="dest", outputCol="dest_index")
+
+# Create a OneHotEncoder
+dest_encoder = OneHotEncoder(inputCol="dest_index", outputCol="dest_fact")
